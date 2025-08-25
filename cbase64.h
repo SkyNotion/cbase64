710bdf5d5fba018326dc5bca6c6fc098405f8727
SkyNotion <meshdrew5@gmail.com>
2025-08-25 15:24:13 +0100

    fix: Added string null terminator to to fix random characters at the end of strings

--- cbase64.h
+++ cbase64.h
@@ -36,13 +36,15 @@ static int base64_padding[3] = {0, 2, 1};
 char* base64_encode_b64ch(char* bin, size_t* text_len, size_t* text_size, char* buffer){
 	int padding = *text_len % 3;
 	size_t block_size = (*text_size + ((*text_size % 6) > 0 ? 6 - (*text_size % 6) : 0)) / 6;
-	char* b64_text = (char*)malloc((block_size * sizeof(char)) + base64_padding[padding] + 1);
+	size_t b64_text_len = (block_size * sizeof(char)) + base64_padding[padding] + 1;
+	char* b64_text = (char*)malloc(b64_text_len);
 	for(size_t i = 0;i < block_size;i++){
 		memset(buffer, '0', 8);
 		memcpy(&buffer[2], &bin[i*6], (*text_size - (i * 6)) < 6 ? (*text_size - (i * 6)): 6);
 		b64_text[i] = base64_encode_lut[strtol(buffer, NULL, 2)];
 	}
 	memset(&b64_text[block_size], '=', base64_padding[padding]);
+	b64_text[b64_text_len - 1] = '\0';
 	free(bin);
 	return b64_text;
 }
@@ -86,7 +88,7 @@ char* base64_encode_file(FILE *file){
 		x++;
 	}
 	fclose(file);	
-	return base64_encode_b64ch(bin, &text_len, &text_size, buffer);;
+	return base64_encode_b64ch(bin, &text_len, &text_size, buffer);
 }
 
 char* base64_decode_b64ch(char* b64_text){
@@ -95,7 +97,8 @@ char* base64_decode_b64ch(char* b64_text){
 	check_padding:
 		if(b64_text[text_len - 1] == '='){text_len--; goto check_padding;}
 	int n, j;
-	char* bin = (char*)malloc(text_len * 6);
+	size_t bin_len = text_len * 6;
+	char* bin = (char*)malloc(bin_len + 1);
 	for(size_t i = 0;i < text_len;i++){
 		memset(buffer, '0', 8);
 		n = base64_decode_lut[(int)b64_text[i]],
@@ -107,18 +110,20 @@ char* base64_decode_b64ch(char* b64_text){
 		}while(n > 0);
 		memcpy(&bin[i*6], &buffer[2], 6);
 	}
+	bin[bin_len] = '\0';
 	return bin;
 }
 
 char* base64_decode(char* b64_text){
 	char buffer[8];
 	char *bin = base64_decode_b64ch(b64_text);
-	size_t data_len = strlen(bin) >> 3;
-	char *text = (char*)malloc(data_len);
-	for(size_t i = 0;i < data_len;i++){
+	size_t text_len = strlen(bin) >> 3;
+	char *text = (char*)malloc(text_len + 1);
+	for(size_t i = 0;i < text_len;i++){
 		memcpy(buffer, &bin[i*8], 8);
 		text[i] = (char)strtol(buffer, NULL, 2);
 	}
+	text[text_len] = '\0';
 	free(bin);
 	return text;
 }
